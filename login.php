9d8b665ea8d7ae766a27b657760d75fdc9983a46
rahulprasad8089 <58581444+rahulprasad8089@users.noreply.github.com>
2019-12-16 18:03:59 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,15 @@
+
+
+
+
+<div align="center">
+<h1>LOGIN</h1>
+<form name="form"  method="post"  form action="profile.php">
+    <label for="username">User Name</label>
+    <input type="text" name="username" id="username"><br><br>
+    <label for="password">Password</label>
+	<input type="text" name="password" id="password"><br><br>
+	<input type="submit" name="submit" value="Submit">
+</form>
+<div>
+<a href="index.php">Back to home</a>
---
+++ profile.php
@@ -0,0 +1,74 @@
+<?php include "templates/header.php"; ?>
+
+<?php
+
+/**
+ * Function to query information based on 
+ * a parameter: in this case, location.
+ *
+ */
+
+if (isset($_POST['submit'])) {
+    try  {
+        
+        require "../config.php";
+        require "../common.php";
+
+        $connection = new PDO($dsn, $username, $password, $options);
+
+        $sql = "SELECT * 
+                        FROM user
+                        WHERE username=:username and password=:password";
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+		
+        $statement = $connection->prepare($sql);
+        $statement->bindParam(':username', $username, PDO::PARAM_STR);
+		$statement->bindParam(':password', $password, PDO::PARAM_STR);
+        $statement->execute();
+
+        $result = $statement->fetchAll();
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+}
+?>
+<?php require "templates/header.php"; ?>
+        
+<?php  
+if (isset($_POST['submit'])) {
+    if ($result && $statement->rowCount() > 0) { ?>
+        <h2>Results</h2>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Username</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Password</th>
+                    <th>Mobile No</th>
+                    <th>Date</th>
+                </tr>
+            </thead>
+            <tbody>
+        <?php foreach ($result as $row) { ?>
+            <tr>
+                <td><?php echo escape($row["id"]); ?></td>
+                <td><?php echo escape($row["username"]); ?></td>
+                <td><?php echo escape($row["name"]); ?></td>
+                <td><?php echo escape($row["email"]); ?></td>
+                <td><?php echo escape($row["password"]); ?></td>
+                <td><?php echo escape($row["mobileno"]); ?></td>
+                <td><?php echo escape($row["date"]); ?> </td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+    <?php } else { ?>
+        <blockquote>No results found for <?php echo escape($_POST['username']); ?>.</blockquote>
+    <?php } 
+} ?> 
+<a href="index.php">Back to home</a>
